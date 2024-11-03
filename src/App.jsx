4c5601c1f8f47a0cf8a831058de7b1973d1f4a8e
Amar Prakash <prakashamar730@gmail.com>
2024-11-03 15:36:49 +0530

    Add Initial file

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+// import './App.css'
+import WeeatherApp from "./WeatherApp";
+
+function App() {
+  function handleClick() {
+    console.log("Button was clicked!");
+  }
+
+  return (
+    <>
+    <WeeatherApp />
+      
+    </>
+  )
+}
+
+export default App
